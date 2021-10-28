c76d728add7e64b5271b58704cfd255c440d2d4b
Marcos <marcosbastidas301@gmail.com>
2021-10-28 11:32:15 -0400

    adding-controller-create-user

---
+++ crontrollers/usersCrontroller.js
@@ -0,0 +1,6 @@
+
+
+
+exports.createUser = (req, res) => {
+
+} 
--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const usersControllers = require('../crontrollers/usersCrontroller');
 //crear usuario
 //api usuarios
 router.post('/', () =>{
-
+    usersControllers.createUser
 })
 
 module.exports = router;
