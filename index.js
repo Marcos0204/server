f9d3d5ae2521da04c7264b4ba185c95c195ef11a
Marcos <marcosbastidas301@gmail.com>
2021-10-27 22:54:57 -0400

    creating-server-express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('iniciando el server... init')
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+
+//define la ruta principal 
+
+app.get('/', (req, res)=>{
+    res.send('hola mundo')
+})
+
+//Arrarcar la app
+app.listen(PORT, () =>{
+    console.log(`La aplicaicon esta funcionando en el puerto ${PORT}`)
+})
